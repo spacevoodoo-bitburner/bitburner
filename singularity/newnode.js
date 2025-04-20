07be10705196807f136da39b5ec261cff861e42c
spacevoodoo-bitburner <achristiananarchist@gmail.com>
2025-04-19 21:21:50 -0500

    new node startup logic

--- singularity/newnode.js
+++ singularity/newnode.js
@@ -1,4 +1,10 @@
 export async function main(ns){
     ns.singularity.commitCrime("Mug");
-    ns.exec("/hive/swarm.js", "home", 1, 8192, 1, 2, 5000);
+    ns.exec("/singularity/nodestartup.js", "home", 1, 8192, 1, 2, 5000);
+    while (ns.getServerMaxRam("home") < 256){
+        await ns.sleep(1000);
+    }
+    ns.exec("/basic/killall.js", "home");
+    ns.killall("home", true);
+    ns.exec("/singularity/spinup.js", "home");
 }
---
+++ singularity/nodestartup.js
@@ -0,0 +1,4 @@
+export async function main(ns){
+    ns.singularity.commitCrime("Mug");
+    ns.exec("/hive/swarm.js", "home", 1, 8192, 1, 2, 5000);
+}
