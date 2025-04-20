fbcaaf3bec88c272d5a43026fb7160380f249bdb
spacevoodoo-bitburner <achristiananarchist@gmail.com>
2025-04-19 21:29:54 -0500

    minor

--- singularity/newnode.js
+++ singularity/newnode.js
@@ -1,5 +1,4 @@
 export async function main(ns){
-    ns.singularity.commitCrime("Mug");
     ns.exec("/singularity/nodestartup.js", "home", 1, 8192, 1, 2, 5000);
     while (ns.getServerMaxRam("home") < 256){
         await ns.sleep(1000);
